267473dfb4f7448826a94f4c265e0ab36ebd6abb
Jose Wilson da Silva Evangelista <76742278+WilsonSe454@users.noreply.github.com>
2023-07-26 19:39:08 -0300

    Básico I

--- basico/projeto/index.js
+++ basico/projeto/index.js
@@ -1,3 +1,36 @@
 console.log('Olá mundo!');
-//console.log('Olá mundo!');
-console.log('Olá mundo!');
+//console.log('Olá mundo!'); comentário de linha
+
+console.log("Olá mundo!");
+/** 
+ * comentário de bloco
+ * 
+ * console.log("Olá mundo!");
+ * console.log("Olá mundo!");
+ * console.log("Olá mundo!");
+ * console.log("Olá mundo!");
+ * console.log("Olá mundo!");
+ */
+
+// Variáveis
+var olaMundo = "Hello World!"; //var é global
+let helloWorld = "Olá Mundo!"; //let é local
+console.log(olaMundo);
+console.log(olaMundo);
+console.log(helloWorld);
+console.log(helloWorld);
+
+
+let a = 10; // = atribuição
+const b = "10"; // = atribuição
+//Valores iguais sem considerar o tipo da variável
+console.log(a == b); //true
+
+//valor e tipo de variável identico
+console.log(a === b); //false
+
+//Valores diferentes sem considerar o tipo da variável
+console.log(a != b); //false
+
+//Valores e tipos de variáveis diferentes
+console.log(a !== b); //true
