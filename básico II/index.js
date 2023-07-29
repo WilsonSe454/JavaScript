2a1f16964c5531019edcb111d825d1443ff85587
Jose Wilson da Silva Evangelista <76742278+WilsonSe454@users.noreply.github.com>
2023-07-29 17:33:31 -0300

    Básico II

--- básico II/index.js
+++ básico II/index.js
@@ -1,13 +1,23 @@
 /**
- * Classes agrupa
- * Atributos e métodos no mesmo lugar
- * 
  * return 
- * Encerra a função e retorna um valor
+ * Encerra a função e retorna um valor.
  * 
- * Argumentos ou parâmetros são informações que as funções precisam
+ * Argumentos ou parâmetros são informações que as funções precisam.
  * 
- * Argumentos ou Args permitem acessar parâmetros de uma função
+ * Argumentos ou Args permitem acessar parâmetros de uma função.
+ * 
+ * callback
+ * Função de retorno como parâmetro após uma ação.
+ * 
+ * DOM
+ * Document Object Model é a estrutura como o HTML é formado.
+ * 
+ * Eventos
+ * Ações realizadas na aplicação que dispara rotinas.
+ * 
+ * Array 
+ * Array é uma variável indexada, no caso do JavaScript é possível ter um array com vários tipos de dados diferentes, até funções.
+ * 
 */
 
 // Funções
@@ -41,3 +51,59 @@ console.log(resultado); // 17
     console.log(resultado); // 7
     return resultado;
 })(3, 4, "+"); // Argumento da função anônima
+
+
+
+// Arrow Function
+let calc = (x1, x2, operador) => { // A variável calc recebe a arrow function
+    // A função eval, avalia o código JavaScript e o executa.
+    return eval(`${x1} ${operador} ${x2}`);
+}
+
+resultado = calc(8, 9, "*"); //Arrow Function e seus Argumentos
+console.log(resultado);
+
+
+//Eventos
+
+// window manipulamos a aplicação
+window.addEventListener('focus', event => {// Evento de foco, com este evento é possível saber quando o usuário voltou a página  
+
+    console.log("focus");
+
+});
+
+// document manipulados a DOM
+document.addEventListener('click', event => { // Evento de click, com este evento é possível saber quando o usuário clica na página  
+    console.log("click");
+});
+
+
+//TimeStamp é a quantidade de segundos desde 01/01/1970
+// let agora = Date.now(); // Date.now(); vai trazer a quantidade de segundos desde 01/01/1970
+let agora = new Date(); // já new Date(); vai mostrar a data e a hora atuais.
+console.log(agora);
+
+console.log(agora.getDate()); // Pega o dia
+console.log(agora.getFullYear()); // Pega o ano
+
+console.log(agora.getMonth()); // Pega o mês, detalhe: é um array de meses, então começa com zero.
+console.log(agora.toLocaleDateString("pt-BR")); // Pega a data no formato do Brasil
+
+//Array é uma variável indexada, no caso do JavaScript é possível ter um array com dados diferentes.
+let carros = ["Palio", "Toro", "Uno", 10, true, new Date(), function calcular(x1, x2, operador) {
+                                                                return eval(`${x1} ${operador} ${x2}`); 
+                                                            }
+]; // Função calcular está dentro do array carros
+
+console.log(carros); // (7) ['Palio', 'Toro', 'Uno', 10, true, Sat Jul 08 2023 12:35:25 GMT-0300 (Horário Padrão de Brasília), ƒ]
+console.log(carros.length); // 7 Mostra o tamanho do array 
+console.log(carros[0]); // Palio, Mostra o primeior item do array 
+
+console.log(carros[6](5, 5, "*")); // Usei a função calcular que está dentro do array carros
+
+carros.forEach(function(value, index){
+
+    console.log(index, value);
+
+});
---
+++ básico II/index2.js
@@ -0,0 +1,58 @@
+/**
+ * Classes 
+ * Agrupa atributos e métodos no mesmo lugar.
+ * Irá englobar todos os assuntos em comum.
+ * 
+ * Instância 
+ * É quando o objeto representa uma classe 
+ * 
+ * this
+ * É um comando interno usado para referenciar atributos e métodos na mesma classe.
+  */
+
+
+console.log("----------------------Jeito Antigo------------------------");
+let celular = function() {
+    //This É um comando interno usado para referenciar atributos e métodos na mesma classe.
+    this.cor = "prata"; 
+
+    this.ligar = function() {
+
+        console.log("Uma ligação");
+        return "Ligando";
+
+    }
+
+}
+
+let objeto = new celular(); // Cria a instância de um objeto
+console.log(objeto); // Mostra o objeto celular
+console.log(objeto.cor); // Mostra a cor do objeto celular
+console.log(objeto.ligar()); // Mostra a cor do objeto celular
+
+console.log("----------------------Jeito Novo-------------------------");
+
+
+class celularN {
+
+    constructor() {
+        // Atributos
+        this.cor = "Azul";
+
+    }
+
+    // Métodos
+    ligar = function () {
+
+        console.log("Recebendo uma Ligação");
+        return "Atender";
+
+    }
+
+}
+
+let objetoN = new celularN(); // Toda vez que coloca o conjunto de parênteses, é chamado o método construtor automaticamente.
+console.log(objetoN); // Mostra o objetoN celularN
+console.log(objetoN.cor); // Mostra a cor do objetoN celularN
+console.log(objetoN.ligar()); // Mostra a cor do objetoN celularN
+
