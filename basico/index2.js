6e2fae3f966c82517aa1c752572e4942e6f9884a
Jose Wilson da Silva Evangelista <76742278+WilsonSe454@users.noreply.github.com>
2023-07-29 10:12:44 -0300

    Template String

--- basico/index2.js
+++ basico/index2.js
@@ -19,3 +19,21 @@ switch (cor) {
         // não utiliza o break 
 
 }
+
+// Laço de repetição
+
+/* 
+For(valor inicial; valor final; incremento){
+    instrução
+}
+ */
+
+let n = 5;
+
+for(let i = 0; i <= 10; i++){
+    // Template String é um recurso do JavaScript que permite a inserção de códigos JS junto a uma string usando a crase `${js}`. 
+    console.log(`${n} X ${i} = ${n*i}`); 
+
+    // Sem este recurso seria necessário concatenar os valares dentro do console.log
+    // console.log(n+" X "+ i + " = " + n * i);
+}
